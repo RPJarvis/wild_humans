e5293369834bd0459b0ecada58188a2d8d7e614b
Ryan Jarvis <rjarvis1@cnm.edu>
2016-02-13 01:08:18 -0700

    social links

--- wild_humans/wild_humans/urls.py
+++ wild_humans/wild_humans/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('wild_humans_main.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
