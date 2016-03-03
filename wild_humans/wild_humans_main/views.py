cff4a977a0951746dcde6833e4e73c2919760d18
JARVIS <RJarvis1@cnm.edu>
2016-03-03 11:21:26 -0700

    latest blog on homepage

--- wild_humans/wild_humans_main/views.py
+++ wild_humans/wild_humans_main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from blog.models import Blog
 
 def index(request):
     context = RequestContext(request)
 
-    context_dict = {}
+ #   last_three_posts = Blog.objects.latest()
+    latest_blog = Blog.objects.latest('publish_date')
+
+    context_dict = {'latest_blog': latest_blog}
 
     return render_to_response('wild_humans_main/index.html', context_dict, context)
