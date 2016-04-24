2cac05e0348cfe21ae515a6c3944019f0708bc71
Julian Singer-Corbin <cherieokeefe@Cherie-OKeefes-MacBook-Pro.local>
2016-04-23 22:20:07 -0600

    almost contact

--- wild_humans/wild_humans/settings.py
+++ wild_humans/wild_humans/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'social_media_links',
     'blog',
     'event_calendar',
+
 ]
 
 MIDDLEWARE_CLASSES = [
--- wild_humans/wild_humans_main/urls.py
+++ wild_humans/wild_humans_main/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 urlpatterns = [
    # url(r'^admin/', include(admin.site.urls)),
     url(r'^listen', views.listen, name='listen'),
+    url(r'^contact/', include('contact_form.urls')),
     url(r'^$', views.index, name='index'), #this should be last
 
 ]
