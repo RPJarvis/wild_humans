e40af7e56c5bbf9447e920a9f513e408be0fb86e
JARVIS <RJarvis1@cnm.edu>
2016-08-05 13:32:12 -0600

    template for images works

--- wild_humans/wild_humans_main/views.py
+++ wild_humans/wild_humans_main/views.py
@@ -2,16 +2,25 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from blog.models import Blog
 from event_calendar.models import Event
+from cover_page.models import CoverImage
+
 
 def index(request):
     context = RequestContext(request)
 
  #   last_three_posts = Blog.objects.latest()
     latest_blog = Blog.objects.latest('publish_date')
-    #TODO: only graby next few events
     events = Event.objects.all()
+    cover_images = CoverImage.objects.all()
+    print cover_images
+    print len(cover_images)
+    if len(cover_images) < 3:
+        num_images = range(1, 2)
+    else:
+        num_images = range(1, len(cover_images) - 1)
+    print 'num images {}'.format(num_images)
 
-    context_dict = {'latest_blog': latest_blog, 'events': events}
+    context_dict = {'latest_blog': latest_blog, 'cover_images': cover_images, 'num_images': num_images, 'events': events}
 
     return render_to_response('wild_humans_main/index.html', context_dict, context)
 
