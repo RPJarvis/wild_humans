ca1f33f6e50c3c9005eaf9208ea309c01799b69b
JARVIS <RJarvis1@cnm.edu>
2016-10-06 13:51:57 -0600

    combine content

--- wild_humans/event_calendar/views.py
+++ wild_humans/event_calendar/views.py
@@ -4,6 +4,7 @@ from models import Event
 def index(request):
     events = Event.objects.all().exclude(passed=True)
     passed_events = Event.objects.all().exclude(passed=False)
+    print passed_events
     context_dict = {'events': events, 'passed_events': passed_events}
 
     return render(request, 'calendar.html', context_dict)
